be17862afbf58a97240d9ebaae275cd0f973d3b7
Uniqcoda <etokwudomary@gmail.com>
2023-04-05 03:24:53 +0100

    Add login screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/Home';
+import Login from './screens/LoginScreen';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
+  // const user = {};
+  const user = null;
   return (
     <div className='app'>
-      <HomeScreen />
+      <Router>
+        <Routes>
+          {!user ? (
+            <Route path='/login' element={<Login />} />
+
+          ) : (
+              <Route path='/' element={<HomeScreen />} />
+          )}
+        </Routes>
+      </Router>
     </div>
   );
 }
