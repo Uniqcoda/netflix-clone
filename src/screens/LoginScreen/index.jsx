959573dac5651a1bd771c184ba27335c8bce60af
Uniqcoda <etokwudomary@gmail.com>
2023-04-15 22:50:58 +0100

    Add form validation for login and sign up

--- src/screens/LoginScreen/index.jsx
+++ src/screens/LoginScreen/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import NetflixLogo from '../../assets/Netflix-logo.png';
 import SingUpScreen from '../SingUpScreen';
 
@@ -6,11 +6,31 @@ import './index.css';
 
 function LoginScreen() {
   const [signIn, setSignIn] = useState(false);
+  const [email, setEmail] = useState(null);
+  const emailRef = useRef(null);
+
+  const validateEmail = (email) => {
+    // Validate email using a regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email.match(emailRegex)) {
+      alert('Please enter a valid email address');
+      return false;
+    }
+    return true;
+  };
+  const loadForm = (e) => {
+    e.preventDefault();
+    const emailValue = emailRef.current.value;
+    if (!validateEmail(emailValue)) return;
+    setEmail(() => emailValue);
+    setSignIn(true);
+  };
 
   return (
     <div className='loginScreen'>
       <div className='loginScreen__background'>
         <img src={NetflixLogo} alt='' className='loginScreen__logo' />
+        {/* Sign In Nav Button */}
         {!signIn && (
           <button className='loginScreen__button' onClick={() => setSignIn(true)}>
             Sign In
@@ -20,7 +40,7 @@ function LoginScreen() {
       </div>
       <div className='loginScreen__body'>
         {signIn ? (
-          <SingUpScreen />
+          <SingUpScreen email={email} />
         ) : (
           <>
             <h1>Unlimited films, TV programmes and more.</h1>
@@ -28,8 +48,8 @@ function LoginScreen() {
             <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
             <div className='loginScreen__input'>
               <form>
-                <input type='email' name='' id='' placeholder='Email Address' />
-                <button className='loginScreen__getStarted' onClick={() => setSignIn(true)}>
+                <input ref={emailRef} type='email' placeholder='Email' name='email' required />
+                <button className='loginScreen__getStarted' onClick={loadForm}>
                   GET STARTED
                 </button>
               </form>
--- src/screens/SingUpScreen/index.jsx
+++ src/screens/SingUpScreen/index.jsx
@@ -4,21 +4,39 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'fire
 import { auth } from '../../utils/firebase';
 import './index.css';
 
-function SignUpScreen() {
-  const emailRef = useRef(null);
+function SignUpScreen({ email }) {
+  const emailRef = useRef(email);
   const passwordRef = useRef(null);
   const navigate = useNavigate();
 
+  const validateForm = (email, password) => {
+    // Validate email using a regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email.match(emailRegex)) {
+      alert('Please enter a valid email address');
+      return false;
+    }
+    if (password.length < 6) {
+      alert('Password should be at least 6 characters');
+      return false;
+    }
+    return true;
+  };
+
   const register = (e) => {
     e.preventDefault();
-    createUserWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
+    const emailValue = emailRef.current.value;
+    const passwordValue = passwordRef.current.value;
+
+    if (!validateForm(emailValue, passwordValue)) return;
+
+    createUserWithEmailAndPassword(auth, emailValue, passwordRef.current.value)
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
         if (user) navigate('/');
       })
       .catch((error) => {
-        // const errorCode = error.code;
         const errorMessage = error.message;
         alert(errorMessage);
       });
@@ -26,15 +44,18 @@ function SignUpScreen() {
 
   const signIn = (e) => {
     e.preventDefault();
+    const emailValue = emailRef.current.value;
+    const passwordValue = passwordRef.current.value;
 
-    signInWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
+    if (!validateForm(emailValue, passwordValue)) return;
+
+    signInWithEmailAndPassword(auth, emailValue, passwordValue)
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
         if (user) navigate('/');
       })
       .catch((error) => {
-        // const errorCode = error.code;
         const errorMessage = error.message;
         alert(errorMessage);
       });
@@ -44,8 +65,8 @@ function SignUpScreen() {
     <div className='signUpScreen'>
       <form>
         <h1>Sign In</h1>
-        <input ref={emailRef} type='email' placeholder='Email' name='email' id='email' />
-        <input ref={passwordRef} type='password' placeholder='Password' name='password' id='password' />
+        <input ref={emailRef} defaultValue={email} type='email' placeholder='Email' name='email' id='email' required />
+        <input ref={passwordRef} type='password' placeholder='Password' name='password' id='password' required />
         <button type='submit' onClick={signIn}>
           Sign In
         </button>
